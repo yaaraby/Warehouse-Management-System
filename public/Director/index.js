322cf999ef35b025ddcd52ace02a63828e4c9a3c
YaaraBy <yaarabenyosef@gmail.com>
2021-02-04 16:28:08 +0200

    cont

--- public/Director/index.js
+++ public/Director/index.js
@@ -469,7 +469,7 @@ function PullThiscCategory(event) {
                 <td>${elm.UPS}</td>
                 <td class="flexCrudProduct">
                 <div class="list" onclick="editProduct('${elm._id}')"><img src="/img/edit-button.png"></div>
-                <div class="list" onclick="deleteProduct('${elm._id}')"><img src="/img/deleteuser.png"></div>
+                <div class="list" onclick="deleteProduct('${elm._id}', '${event}' )"><img src="/img/deleteuser.png"></div>
                 </td>
         </tr>
 `).join('')}</tbody>
@@ -987,8 +987,7 @@ const Validations = (UPS, name, price, amount, category, weight, height, ExpiryD
 }
 
 
-const deleteProduct = (_id, e) => {
-    console.log(e)
+const deleteProduct = (_id) =>{
     console.log(_id)
  fetch('/deleteProduct/' + _id, {
         method: 'DELETE',
