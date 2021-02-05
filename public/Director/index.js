1f52efcca8f132d6760a0dbb1864f5c126f508b1
YaaraBy <yaarabenyosef@gmail.com>
2021-02-05 14:18:15 +0200

    cont

--- public/Director/index.js
+++ public/Director/index.js
@@ -1003,7 +1003,67 @@ const deleteProduct = (_id) =>{
         })
 }
 
- 
+const editProduct = (id) =>{
+    menubutoondisplayblock()
+    letdistinctResult= []; 
+    fetch('/get-details-product' + id,{
+              method: 'GET',
+           headers: {
+               'Content-Type': 'application/json'
+           }
+       }).then(res =>
+           res.json()
+       )
+       .then(data => {
+           console.log(data)
+            document.getElementById('editProductById').innerHTML =
+            editProductById.style.display = "block"     
+                   `<h1>עריכת מוצר</h1>
+                   <form onsubmit="handleEditProduct(event, '${data.Amount}', '${data.Weight}','${data.Location}')">
+                   
+                <div class="productDetails">
+                    <label for="UPS">מק"ט:
+                   <input type="number" name="UPS" id="UPS" value="${data.UPS}" disabled="disabled" autocomplete='off'></br>
+               </label>
+               <label for="Name">שם:
+                   <input type="text" name="Name" id="Name" value="${data.Name}" autocomplete='off'></br>
+               </label>
+               <label for="price">מחיר:
+                   <input type="text" name="price" id="price" value=${data.price} autocomplete='off'></br>
+               </label>
+               <label for="Amount">כמות:
+                   <input type="number" name="Amount" id="Amount" value=${data.Amount} autocomplete='off'></br>
+               </label>
+               <label for="Category">קטגוריה:
+                   <input type="text" name="Category" id="Category" value=${data.Category} autocomplete='off'></br>
+               </label>
+               <label for="Weight">משקל:
+                   <input type="number" name="Weight" id="Weight" value=${data.Weight} autocomplete='off'></br>
+               </label>
+                <label for="height">גובה:
+                   <input type="number" name="height" id="height" value=${data.height} autocomplete='off'></br>
+               </label>
+               <label for="ExpiryDate">תאריך תפוגה:
+                   <input type="date" name="ExpiryDate" id="ExpiryDate" value=${data.ExpiryDate} autocomplete='off'></br>
+               </label>
+           </div>
+            <select name='Location' id='Location'>
+            <option value = ${data.Location}> ${data.Location} </option>
+            </select></br>
+           <div id="message"></div></br>
+           <input type="submit" value="אישור">
+       </form>`;
+   // console.log(this.shelfOptions)
+   document.getElementById("Location").innerHTML = this.shelfOptions.join(" ");
+
+       }).catch(err => {
+           console.error(err);
+       }).finally(() => {
+           console.log('im done')
+       } )
+    
+} 
+ /* 
 const editProduct = (id) =>{
     menubutoondisplayblock()
     letdistinctResult= []; 
@@ -1067,7 +1127,7 @@ const editProduct = (id) =>{
            console.log('im done')
        } )
     
-} 
+}  */
 
 
 async function handleEditProduct(e, PreviosAmount, PreviosWeight, PreviosLocation) {
